646570ff639dc30654c45c9763f2793b6a47659b
Rafael Araujo <rafaelaraujo1707@gmail.com>
2022-10-19 14:06:17 -0300

    feat: add middleware token authentication

--- src/modules/main/routes/specifications-routes.ts
+++ src/modules/main/routes/specifications-routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { specificationsController } from "../../cars/usecases/create-specification.ts/index";
 import { SpecificationsRoutesAdapter } from "../providers/express/implementations/express-adapter";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const specificationsRoutes = Router();
 
 specificationsRoutes.post(
   "/",
+  ensureAuthenticated,
   SpecificationsRoutesAdapter(specificationsController)
 );
 
