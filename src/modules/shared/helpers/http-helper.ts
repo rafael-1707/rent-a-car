d683c7b53aef630d99a563677957cdc256e01050
Rafael Araujo <rafaelaraujo1707@gmail.com>
2022-10-19 14:07:08 -0300

    feat: added more errors

--- src/modules/shared/helpers/http-helper.ts
+++ src/modules/shared/helpers/http-helper.ts
@@ -14,3 +14,23 @@ export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data,
 });
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: error,
+});
+
+export const unauthorized = (error: Error): HttpResponse => ({
+  statusCode: 401,
+  body: error,
+});
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error,
+});
+
+export const notFound = (error: Error): HttpResponse => ({
+  statusCode: 404,
+  body: error,
+});
